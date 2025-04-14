1cdb3b7f9895e01e68faf23782ef94f1d73f7c5e
Debaswini Mishra <115451804+Debaswinimishra@users.noreply.github.com>
2025-04-14 14:20:30 +0530

    link added

--- src/pages/index.js
+++ src/pages/index.js
@@ -53,7 +53,7 @@ const Page = () => {
     console.log("Child Name:", childName);
     console.log("Class:", selectedClass);
     console.log("Selected Participant:", searchParticipant);
-    window.location.href = "whatsapp://app";
+    window.location.href = "whatsapp://";
   };
 
   return (
